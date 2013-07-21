0000bb75dd1fbb7e1a6b84a442101bb46d2c7f20
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-07-21 21:11:38 +0300

    Run all runners on specified repo

--- lib/pronto.rb
+++ lib/pronto.rb
@@ -4,9 +4,12 @@ require 'pronto/message'
 require 'pronto/runner'
 
 module Pronto
-  def self.register_runner(runner)
-    @runners ||= []
-    @runners << runner
-  end
+  def self.run(start_commit, end_commit = 'master', repo_path = '/')
+    repo = Grit::Repo.new(repo_path)
+    diffs = repo.diff(start_commit, end_commit)
 
+    Runner.runners.map do |runner|
+      runner.new.run(diffs)
+    end.flatten.compact
+  end
 end
