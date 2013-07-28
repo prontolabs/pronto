77d3e4e6981f315ed0f1ff4a20d57a47bb5a45ee
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-07-28 20:39:51 +0300

    Add 'version' cli command

--- lib/pronto/cli.rb
+++ lib/pronto/cli.rb
@@ -2,5 +2,14 @@ require 'thor'
 
 module Pronto
   class CLI < Thor
+    require 'pronto'
+    require 'pronto/version'
+
+    desc 'version', 'Show the Pronto version'
+    map %w(-v --version) => :version
+
+    def version
+      puts "Pronto version #{::Pronto::VERSION}"
+    end
   end
 end
