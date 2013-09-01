d79b67178d1e195f5363e7ac1f29d9c1374d1b1d
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-09-01 21:27:55 +0300

    Add new_file_full_path method on Rugged::Diff::Delta

--- lib/rugged/diff/delta.rb
+++ lib/rugged/diff/delta.rb
@@ -1,25 +1,13 @@
 module Rugged
   class Diff
     class Delta
-      def new_blob
-        blob(new_file)
-      end
-
-      def old_blob
-        blob(old_file)
-      end
-
       def repo
         diff.tree.repo
       end
 
-      private
-
-      def blob(file)
-        return if file.nil?
-        return if file[:oid] == '0000000000000000000000000000000000000000'
-
-        repo.lookup(file[:oid])
+      def new_file_full_path
+        repo_path = Pathname.new(repo.path).parent
+        repo_path.join(new_file[:path])
       end
     end
   end
