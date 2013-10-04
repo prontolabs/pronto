a64eb6dc64216c2fa5a0ea04b95f184536796c2a
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-10-04 20:18:30 +0300

    Test5

--- lib/pronto/formatter/github_formatter.rb
+++ lib/pronto/formatter/github_formatter.rb
@@ -4,6 +4,8 @@ module Pronto
   module Formatter
     class GithubFormatter
       def format(messages)
+        puts "#{messages.count} messages discovered by Pronto"
+
         commit_messages = messages.map do |message|
           repo = github_slug(message)
           sha = message.line.commit_sha
@@ -19,6 +21,10 @@ module Pronto
 
       private
 
+      def renaem_create_comment(repo, sha, position, path, body)
+        "TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"
+      end
+
       def create_comment(repo, sha, position, path, body)
         "TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"
 
@@ -31,21 +37,13 @@ module Pronto
         end
 
         unless existing_comment
+          puts "commit: #{sha}, #{body}, #{sha}, #{path}, #{position}"
           client.create_commit_comment(repo, sha, body, path, nil, position)
         end
       end
 
-      def create_comment(repo, sha, position, path, body)
+      def teterenaem_create_comment(repo, sha, position, path, body)
         "TESTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT"
-
-        comments = @pull_id ? client.pull_comments(repo, @pull_id)
-                            : client.commit_comments(repo, sha)
-
-        if @pull_id
-          client.create_pull_comment(repo, @pull_id, body, sha, path, position)
-        else
-          client.create_commit_comment(repo, sha, body, path, nil, position)
-        end
       end
 
       def access_token
