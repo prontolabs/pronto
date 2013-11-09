94a266ee1343b78f4f0b180b690f03058108a8ce
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-11-09 23:11:20 +0200

    Ignore not-commited changes when running blame, fixes #15

--- lib/pronto/rugged/repository.rb
+++ lib/pronto/rugged/repository.rb
@@ -5,7 +5,7 @@ module Rugged
     def blame(filepath)
       # TODO: Using grit blame implementation for now.
       # Replace it with Rugged implementation when it's available.
-      ::Grit::Repo.new(path).blame(filepath)
+      ::Grit::Repo.new(path).blame(filepath, 'HEAD')
     end
   end
 end
