65ce672b3073fd391f83cb1a6c716d4493cbce3b
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2014-07-13 21:22:21 +0300

    Passing parents.first is no longer necessary for diff

--- lib/pronto/rugged/commit.rb
+++ lib/pronto/rugged/commit.rb
@@ -2,8 +2,7 @@ module Rugged
   class Commit
     def show
       # TODO: Rugged does not seem to support diffing against multiple parents
-      return if parents.count != 1
-      diff(parents.first, reverse: true)
+      diff(reverse: true) unless parents.count != 1
     end
   end
 end
