345b836eacfc73dc026215e37f45fd7457d81650
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-04-19 13:47:54 +0300

    Add more specs for Pronto::Message

--- spec/pronto/message_spec.rb
+++ spec/pronto/message_spec.rb
@@ -2,17 +2,46 @@ require 'spec_helper'
 
 module Pronto
   describe Message do
+    let(:message) { Message.new(path, line, level, msg, '8cda581') }
+
+    let(:path) { 'README.md' }
+    let(:line) { Git::Line.new }
+    let(:msg) { 'message' }
+    let(:level) { :warning }
+
     describe '.new' do
-      subject { Message.new(path, line, level, msg, '8cda581') }
-      let(:path) { 'README.md' }
-      let(:line) { Rugged::Diff::Line.new }
-      let(:msg) { 'message' }
+      subject { message }
 
       Message::LEVELS.each do |message_level|
         context "set log level to #{message_level}" do
           let(:level) { message_level }
           its(:level) { should == message_level }
         end
+      end
+
+      context 'bad level' do
+        let(:level) { :random }
+        specify do
+          lambda { subject }.should raise_error
+        end
+      end
+    end
+
+    describe '#full_path' do
+      subject { message.full_path }
+
+      context 'line is nil' do
+        let(:line) { nil }
+        it { should be_nil }
+      end
+    end
+
+    describe '#repo' do
+      subject { message.repo }
+
+      context 'line is nil' do
+        let(:line) { nil }
+        it { should be_nil }
       end
     end
   end
