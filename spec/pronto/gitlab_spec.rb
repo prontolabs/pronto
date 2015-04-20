3bffa0f4bc7d79a776bfbc30ba8f2bffad9db475
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-04-20 22:43:03 +0300

    Improve Gitlab spec

--- spec/pronto/gitlab_spec.rb
+++ spec/pronto/gitlab_spec.rb
@@ -10,16 +10,17 @@ module Pronto
       context 'three requests for same comments' do
         let(:repo) { double(remote_urls: ['git@gitlab.example.com:mmozuras/pronto.git']) }
         let(:sha) { 'foobar' }
+        let(:comment) { double(note: 'note', path: 'path', line: 1) }
 
         specify do
           ENV['GITLAB_API_ENDPOINT'] = 'http://gitlab.example.com/api/v3'
           ENV['GITLAB_API_PRIVATE_TOKEN'] = 'token'
 
           ::Gitlab::Client.any_instance
-                          .should_receive(:commit_comments)
-                          .with('mmozuras%2Fpronto', sha, per_page: 500)
-                          .once
-                          .and_return([])
+            .should_receive(:commit_comments)
+            .with('mmozuras%2Fpronto', sha, per_page: 500)
+            .once
+            .and_return([comment])
 
           subject
           subject
