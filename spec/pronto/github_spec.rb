4fe6eae4b971f58b7d0781e05b17cf4b1758e685
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-04-22 20:46:21 +0300

    Improve Github specs

--- spec/pronto/github_spec.rb
+++ spec/pronto/github_spec.rb
@@ -4,21 +4,20 @@ module Pronto
   describe Github do
     let(:github) { Github.new(repo) }
 
+    let(:repo) { double(remote_urls: ['git@github.com:mmozuras/pronto.git']) }
+    let(:sha) { '61e4bef' }
+    let(:comment) { double(body: 'note', path: 'path', line: 1, position: 1) }
+
     describe '#commit_comments' do
       subject { github.commit_comments(sha) }
 
       context 'three requests for same comments' do
-        let(:repo) do
-          double(remote_urls: ['git@github.com:mmozuras/pronto.git'])
-        end
-        let(:sha) { '61e4bef' }
-
         specify do
           Octokit::Client.any_instance
             .should_receive(:commit_comments)
             .with('mmozuras/pronto', sha)
             .once
-            .and_return([])
+            .and_return([comment])
 
           subject
           subject
@@ -31,17 +30,12 @@ module Pronto
       subject { github.pull_comments(sha) }
 
       context 'three requests for same comments' do
-        let(:repo) do
-          double(remote_urls: ['https://github.com/mmozuras/pronto.git'])
-        end
-        let(:sha) { '61e4bef' }
-
         specify do
           Octokit::Client.any_instance
             .should_receive(:pull_comments)
             .with('mmozuras/pronto', 10)
             .once
-            .and_return([])
+            .and_return([comment])
 
           ENV['PULL_REQUEST_ID'] = '10'
 
