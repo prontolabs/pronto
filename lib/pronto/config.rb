186dca70a71a31680c96167bb8283dc674f81d18
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-10-25 20:41:38 +0200

    Rename config_file to config_hash

--- lib/pronto/config.rb
+++ lib/pronto/config.rb
@@ -9,25 +9,25 @@ module Pronto
     end
 
     def github_access_token
-      ENV['GITHUB_ACCESS_TOKEN'] || @config_file['github']['access_token']
+      ENV['GITHUB_ACCESS_TOKEN'] || @config_hash['github']['access_token']
     end
 
     def github_slug
-      @config_file['github']['slug']
+      @config_hash['github']['slug']
     end
 
     def gitlab_private_token
-      ENV['GITLAB_API_PRIVATE_TOKEN'] || @config_file['gitlab']['private_token']
+      ENV['GITLAB_API_PRIVATE_TOKEN'] || @config_hash['gitlab']['private_token']
     end
 
     def gitlab_endpoint
-      ENV['GITLAB_API_ENDPOINT'] || @config_file['gitlab']['endpoint']
+      ENV['GITLAB_API_ENDPOINT'] || @config_hash['gitlab']['endpoint']
     end
 
     private
 
     def exclude
-      @config_file['all']['exclude']
+      @config_hash['all']['exclude']
     end
   end
 end
