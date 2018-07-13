cca89f4690092d8af35f1e15000b453ba96c999b
DimitrovV <ventsislav.dimitrov@emerchantpay.com>
2018-07-13 09:06:13 +0300

    Extend github formatter specs

--- spec/pronto/formatter/github_formatter_spec.rb
+++ spec/pronto/formatter/github_formatter_spec.rb
@@ -1,54 +1,63 @@
 module Pronto
   module Formatter
     describe GithubFormatter do
+
       let(:formatter) { described_class.new }
 
       describe '#format' do
+
         subject { formatter.format(messages, repo, nil) }
-        let(:messages) { [message, message] }
-        let(:repo) { Git::Repository.new('spec/fixtures/test.git') }
-        let(:message) { Message.new('path/to', line, :warning, 'crucial') }
-        let(:line) { double(new_lineno: 1, commit_sha: '123', position: nil) }
-        before { line.stub(:commit_line).and_return(line) }
 
-        specify do
-          Octokit::Client.any_instance
-            .should_receive(:commit_comments)
-            .once
-            .and_return([])
+        let(:repo)               { Git::Repository.new('spec/fixtures/test.git') }
+        let(:published_comments) { "%{count} Pronto messages posted to #{formatter.pretty_name}" }
 
-          Octokit::Client.any_instance
-            .should_receive(:create_commit_comment)
-            .once
+        context 'with duplicates' do
 
-          subject
-        end
-      end
+          let(:messages) { [message, message] }
+          let(:message)  { Message.new('path/to', line, :warning, 'crucial') }
+          let(:line)     { double(new_lineno: 1, commit_sha: '123', position: nil) }
 
-      describe '#format without duplicates' do
-        subject { formatter.format(messages, repo, nil) }
-        let(:messages) { [message1, message2] }
-        let(:repo) { Git::Repository.new('spec/fixtures/test.git') }
-        let(:message1) { Message.new('path/to1', line1, :warning, 'crucial') }
-        let(:message2) { Message.new('path/to2', line2, :warning, 'crucial') }
-        let(:line1) { double(new_lineno: 1, commit_sha: '123', position: nil) }
-        let(:line2) { double(new_lineno: 2, commit_sha: '123', position: nil) }
-        before do
-          line1.stub(:commit_line).and_return(line1)
-          line2.stub(:commit_line).and_return(line2)
+          before { line.stub(:commit_line).and_return(line) }
+
+          specify do
+            Octokit::Client.any_instance
+              .should_receive(:commit_comments)
+              .once
+              .and_return([])
+
+            Octokit::Client.any_instance
+              .should_receive(:create_commit_comment)
+              .once
+
+            subject.should eq format(published_comments, count: 1)
+          end
         end
 
-        specify do
-          Octokit::Client.any_instance
-            .should_receive(:commit_comments)
-            .once
-            .and_return([])
+        context 'without duplicates' do
 
-          Octokit::Client.any_instance
-            .should_receive(:create_commit_comment)
-            .twice
+          let(:messages) { [message1, message2] }
+          let(:message1) { Message.new('path/to1', line1, :warning, 'crucial') }
+          let(:message2) { Message.new('path/to2', line2, :warning, 'crucial') }
+          let(:line1)    { double(new_lineno: 1, commit_sha: '123', position: nil) }
+          let(:line2)    { double(new_lineno: 2, commit_sha: '123', position: nil) }
 
-          subject
+          before do
+            line1.stub(:commit_line).and_return(line1)
+            line2.stub(:commit_line).and_return(line2)
+          end
+
+          specify do
+            Octokit::Client.any_instance
+              .should_receive(:commit_comments)
+              .once
+              .and_return([])
+
+            Octokit::Client.any_instance
+              .should_receive(:create_commit_comment)
+              .twice
+
+            subject.should eq format(published_comments, count: 2)
+          end
         end
       end
     end
