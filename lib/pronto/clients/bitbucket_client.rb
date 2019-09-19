3b2ea4531bcb13bd5b2a3a8ff38f33b13bcd2dc3
Phạm Trung Nam <puma.puma07@gmail.com>
2019-09-20 01:41:57 +0700

    Bitbucket deprecated its API v1.0: update all endpoints to API v2.0 (#352)
    * Bitbucket deprecated its API v1.0: update all endpoints to API v2.0
    (pull_requests wasn't updated)
    
    * Fix issue of approve_pull_request and unapprove_pull_request method
    
    * Fix github create pullrequest comment issue
    
    * Fix bitbucket create pullrequest comment issue
    
    * Update bitbucket API v2 pullrequest format
    
    * Update bitbucket API v2 pullrequest format
    
    * Rollback bitbucket.rb and parse new bitbucket comment format
    
    * Rename position to line_to
    
    * Fix duplicate pullrequest comment

--- lib/pronto/clients/bitbucket_client.rb
+++ lib/pronto/clients/bitbucket_client.rb
@@ -8,7 +8,7 @@ class BitbucketClient
 
   def commit_comments(slug, sha)
     response = get("/#{slug}/changesets/#{sha}/comments")
-    openstruct(response['values'])
+    parse_comments(openstruct(response['values']))
   end
 
   def create_commit_comment(slug, sha, body, path, position)
@@ -17,7 +17,7 @@ class BitbucketClient
 
   def pull_comments(slug, pull_id)
     response = get("/#{slug}/pullrequests/#{pull_id}/comments")
-    openstruct(response['values'])
+    parse_comments(openstruct(response['values']))
   end
 
   def pull_requests(slug)
@@ -43,12 +43,28 @@ class BitbucketClient
     response.map { |r| OpenStruct.new(r) }
   end
 
+  def parse_comments(values)
+    values.each do |value|
+      value.content = value.content['raw']
+      value.line_to = value.inline['to']
+      value.filename = value.inline['path']
+    end
+    values
+  end
+  
   def post(url, body, path, position)
     options = {
       body: {
-        content: body,
-        line_to: position,
-        filename: path
+        content: {
+          raw: body
+        },
+        inline: {
+          to: position,
+          path: path
+        }
+      }.to_json,
+      headers: {
+        'Content-Type': 'application/json'
       }
     }
     self.class.post(url, options)
